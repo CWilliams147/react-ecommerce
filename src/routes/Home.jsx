467084b51e0ac403e976d1b636edbff5b55b3c41
Cameron Williams <cswilliams112004@gmail.com>
2024-02-28 21:01:08 -0500

    updates to home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,23 @@
+import "./Home.css";
+
 const Home = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <h1 className="home-page">
+      Home Page
+      <div className="container">
+        <div className="cloud front">
+          <span className="left-front"></span>
+          <span className="right-front"></span>
+        </div>
+        <span className="sun sunshine"></span>
+        <span className="sun"></span>
+        <div className="cloud back">
+          <span className="left-back"></span>
+          <span className="right-back"></span>
+        </div>
+      </div>
+    </h1>
+  );
 };
 
 export default Home;
