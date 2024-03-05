68e7e303bb389283990962912e175fcfc6b22849
Cameron Williams <cswilliams112004@gmail.com>
2024-03-04 20:22:24 -0500

    changes to layout

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -13,23 +13,49 @@ const Products = () => {
 
   return (
     <>
-      <div className="product-list">
-        <h2>Product List</h2>
-        <ul>
-          {data.map((product, index) => {
-            return (
-              <li key={index}>
-                <Link to={Products.id}>
-                  <div className="product-title">{product.title}</div>
-                  <div>
-                    <img className="image" src={product.image} />
-                  </div>
-                  <div className="product-price">Price: {product.price}</div>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
+      <div className="product-list-container">
+        <div className="product-categories">
+          <h2 className="category-header">Categories</h2>
+          <Link to="/category/electronics" className="category-electronics">
+            Electronics
+          </Link>
+          <Link to="/category/jewelery" className="category-jewlery">
+            Jewelery
+          </Link>
+          <Link
+            to="/category/men's clothing"
+            className="category-mens-clothing"
+          >
+            Men's Clothing
+          </Link>
+          <Link
+            to="/category/women's clothing"
+            className="category-womens-clothing"
+          >
+            Women's Clothing
+          </Link>
+        </div>
+        {/* <h2>Product List</h2> */}
+        <div className="product-list">
+          <ul>
+            {data.map((product, index) => {
+              return (
+                <li className="product-card" key={index}>
+                  <Link to={`/products/${product.id}`}>
+                    <div className="product-title">{product.title}</div>
+                    <div>
+                      <img className="image" src={product.image} />
+                    </div>
+                    <div className="product-price">Price: {product.price}</div>
+                  </Link>
+                  <button onClick={() => addToCart(product)}>
+                    Add to Cart
+                  </button>{" "}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </>
   );
